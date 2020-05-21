d365ec9b8d029ceeb86bf16deff3569b5e15ae71
Julian Kimmig <julian.kimmig@gmx.net>
2020-05-21 15:59:50 +0200

    compose db setup

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,4 @@ EXPOSE 8000
 
 RUN conda env create --file environment.yml
 
-CMD exec gunicorn chemDB.wsgi:application --bind 0.0.0.0:8001 --workers 3
+CMD exec gunicorn chemDB.wsgi:application --bind 0.0.0.0:8000 --workers 3
--- chemDB/chemDB/settings.py
+++ chemDB/chemDB/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'chemDB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':  os.environ['MYSQL_DATABASE'],
+        'USER':  os.environ['MYSQL_USER'],
+        'PASSWORD': os.environ['MYSQL_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
