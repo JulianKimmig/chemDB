3158638114218406052f7539d0b3e94bd18f0721
Julian Kimmig <julian.kimmig@gmx.net>
2020-05-21 19:53:37 +0200

    gunicorn setup

--- chemDB/manage.py
+++ chemDB/manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    print("yay2",flush=True)
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chemDB.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -18,5 +19,5 @@ def main():
 
 
 if __name__ == '__main__':
-    print("yay")
-#    main()
+    print("yay",flush=True)
+    main()
