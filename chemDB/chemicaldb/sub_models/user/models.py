093c408e694649afc03dde46099532e3eeafddea
Julian Kimmig <julian.kimmig@gmx.net>
2020-05-30 05:46:21 +0200

    related institutes

--- chemDB/chemicaldb/sub_models/user/models.py
+++ chemDB/chemicaldb/sub_models/user/models.py
@@ -5,10 +5,15 @@ from django.db import models
 class ChemdbInstitute(models.Model):
     name = models.CharField(max_length=64)
     short = models.CharField(max_length=4, unique=True)
+    parent_institute = models.ForeignKey("self",on_delete=models.SET_NULL,null=True,related_name="child_institute",blank=True)
 
     def __str__(self):
-        return self.name
+        return "{} ({})".format(self.name,self.get_short())
 
+    def get_short(self):
+        if self.parent_institute:
+            return self.parent_institute.get_short() + "_" + self.short
+        return self.short
 
 class ChemdbUser(models.Model):
     auth_user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -22,7 +27,7 @@ class ChemdbUser(models.Model):
         if self.auth_user.first_name and self.auth_user.last_name:
             name = "{} {}".format(self.auth_user.first_name, self.auth_user.last_name)
             if self.institute:
-                name += " ({})".format(self.institute.short)
+                name += " ({})".format(self.institute.get_short())
             return name
 
         return self.auth_user.username
@@ -30,7 +35,7 @@ class ChemdbUser(models.Model):
     def get_prefix(self):
         s = ""
         if self.institute:
-            s += self.institute.short + "_"
+            s += self.institute.get_short() + "_"
         s += self.short
         return s
 
